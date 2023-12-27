81bf98369871f8ad4bacafa33f21ab25e72dc2b8
YuriTayro <yuri.tayrocnp@gmail.com>
2023-12-26 22:00:51 -0300

    Segundo commit

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>WDEV Vagas!</title>
+  </head>
+  <body class="bg-dark text-light">
+    <div class="container"><!--Essa div vai permitir q o texto fique centralizado na tela. Essa div só vai fechar lá no footer -->
+        <div class="jumbotron bg-danger">
+            <h1>WDEV Vagas</h1>
+            <p>Exemplo de CRUD com PHP orientado a objetos</p>
+        </div>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php'; //usamos o require pq, caso esse arquivo não esteja no projeto, pode dar problema.
+
+include __DIR__.'/includes/header.php';
+include __DIR__.'/includes/listagem.php';
+include __DIR__.'/includes/footer.php';
